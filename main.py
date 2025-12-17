696b31d6ce46966b52c87c795c36ac3d180b3436
Andy Meissner <andy.meissner96@gmail.com>
2025-12-17 01:26:46 +0100

    Implement downloader of puzzle input

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import argparse
+import tools
+
+
 def main():
-    print("Hello from advent-of-code!")
+    parser = argparse.ArgumentParser(
+        prog="advent-of-code", description="Solves advent-of-code problems"
+    )
+    sub = parser.add_subparsers(required=True, dest="command")
+    download = sub.add_parser("download")
+    download.add_argument("--number", required=True, type=int)
+    download.add_argument("--year", default=2025, type=int)
+    arguments = parser.parse_args()
+
+    if arguments.command == "download":
+        tools.download(arguments.year, arguments.number)
 
 
 if __name__ == "__main__":
--- tools.py
+++ tools.py
@@ -1,2 +1,19 @@
+import requests
+
+
+def download(year: int, puzzle_number: int):
+    puzzle_url = f"https://adventofcode.com/{year}/day/{puzzle_number}/input"
+
+    cookies = {
+        "session": "53616c7465645f5f03825fb743e3317d7d25ecbab3ac0a1d0b33c3d385ee1ca2b87fc0f9f2e719d06ef1ff3bb3e68b9a4784905e911e643cc79ac8887eec7665"
+    }
+    headers = {"User-Agent": "andy_meissner"}
+    response = requests.get(puzzle_url, cookies=cookies, headers=headers)
+    response.raise_for_status()
+
+    with open(f"{year}/p{puzzle_number}.txt", "w") as f:
+        f.write(response.text)
+
+
 def get_puzzle_input() -> list[str]:
     return []
